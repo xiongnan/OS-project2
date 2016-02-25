72e981f3936c644150b1a5f43b6c669b020bef75
nanxiong2014@gmail.com <jkl85321>
2016-02-24 17:03:42 -0800

    test

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -141,7 +141,7 @@ pid_t exec (const char *cmd_line)
 {
   check_ptr(cmd_line);
   cmd_line=user_to_kernel_ptr(cmd_line);
-  printf("print cmd_line:%s\n", cmd_line);
+  //printf("print cmd_line:%s\n", cmd_line);
   pid_t pid = process_execute(cmd_line);
   // struct child_process* cp = get_child_process(pid);
   // ASSERT(cp);
